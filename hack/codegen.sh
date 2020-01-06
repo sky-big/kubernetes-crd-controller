aa84919824c2760ed7bd3162f859a4f6c76d8960
sky-big <sky_big@yeah.net>
2020-01-06 20:11:59 +0800

    generate code success

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# work dir
+export WORK_DIR=$(cd `dirname $0`; cd ..; pwd)
+
 go get -u k8s.io/code-generator/...
 
-cd $GOPATH/src/k8s.io/code-generator
-
-./generate-groups.sh "deepcopy,client,informer,lister" \
+bash ${WORK_DIR}/vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/sky-big/kubernetes-crd-controller/pkg/client github.com/sky-big/kubernetes-crd-controller/pkg/apis \
   "example:v1alpha1,v1" \
+  --go-header-file ${WORK_DIR}/hack/boilerplate.go.txt
